b89b4b2df8311ef86d8c690a8d38ed9ec75f90cf
rashidulas <rashidul321alam@gmail.com>
2024-08-05 01:36:04 -0500

    mid

--- src/pages/JobsPage.jsx
+++ src/pages/JobsPage.jsx
@@ -1,10 +1,10 @@
-
+import JobListings from "../components/JobListings"
 
 const JobsPage = () => {
   return (
-    <div>
-      
-    </div>
+    <section className="bg-blue-50 px-4 py-6">
+        < JobListings />
+    </section>
   )
 }
 
